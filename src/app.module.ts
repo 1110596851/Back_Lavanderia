244690f8c74ec97a4fe7c6038c59548b5470c7d7
1110596851 <yesicamirquez@gmail.com>
2025-05-26 10:16:14 -0500

    correccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,44 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './users/entities/user.entity';
+
 import { UserModule } from './users/user.module';
-import { Domicilio } from './domicilios/entities/domicilio.entity';
 import { DomiciliosModule } from './domicilios/domicilios.module';
 import { ContactoModule } from './contacto/contacto.module';
+
+import { User } from './users/entities/user.entity';
+import { Domicilio } from './domicilios/entities/domicilio.entity';
 import { ServiceItem } from './domicilios/entities/servicio.entity';
 import { Contacto } from './contacto/entities/contacto.entity';
 
 @Module({
-  imports: [UserModule, DomiciliosModule, ContactoModule, TypeOrmModule.forRoot({
-      type: 'mariadb', // o 'postgres', 'sqlite', etc.
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'damaris',
-      password: 'o11k07c99y',
-      database: 'lavanderia',
-      entities: [User, Domicilio, ServiceItem, Contacto],
-      synchronize: true, // solo para desarrollo
-    }),],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT', '3306')),
+        username: config.get<string>('DB_USER'),
+        password: config.get<string>('DB_PASS'),
+        database: config.get<string>('DB_NAME'),
+        entities: [User, Domicilio, ServiceItem, Contacto],
+        synchronize: true, // ⚠️ solo para desarrollo
+      }),
+    }),
+    UserModule,
+    DomiciliosModule,
+    ContactoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
